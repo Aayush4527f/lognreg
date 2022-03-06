da4bc148a32af1b75a019d1858f57fb7ad6692fd
Aayush <aayushchhajed@gmail.com>
2022-03-06 21:14:06 +0530

    register with jwt

--- app.js
+++ app.js
@@ -29,21 +29,27 @@ mongoose.connect('mongodb://localhost:27017/jwtlogin', {
 })
 
 // ENDPOINTS
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('HOME')
 })
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.send('ABOUT')
 })
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
-app.get('/login',(req,res)=>{
-    res.render('login.ejs')
+app.get('/login', (req, res) => {
+    // try {
+    //     jwt.verify(req.cookies.token, process.env.access_token_secret)
+    //     res.redirect('/')
+    // } catch (error) {
+    //     console.log(error.message)
+    //     res.render('login.ejs')
+    // }
 })
 
 // post requests
-app.post('/register',async (req,res)=>{
+app.post('/register', async (req, res) => {
     const { username, password: textPassword } = req.body
     const password = await bcrypt.hash(textPassword, 10)
 
@@ -53,7 +59,10 @@ app.post('/register',async (req,res)=>{
             password
         })
         // console.log('User created successfully: ', response)
-        res.redirect(`/login`)
+        let token = jwt.sign({ "username": `${username}` }, process.env.access_token_secret, { expiresIn: "7d" })
+        res.cookie(`token`, `${token}`, { maxAge: (60 * 60 * 24 * 7) * 1000, secure: true })
+        return res.redirect('/')
+
     } catch (error) {
         if (error.code === 11000) {
             // duplicate key
@@ -62,11 +71,26 @@ app.post('/register',async (req,res)=>{
         throw error
     }
 })
-app.post('/login',(req,res)=>{
-    res.send('LOGIN FORM')
+app.post('/login', async(req, res) => {
+    // const { username, password } = req.body
+    // const user = await User.findOne({ username }).lean()
+
+    // if (!user) {
+    //     return res.send('Invalid username/password')
+    // }
+
+    // if (await bcrypt.compare(password, user.password)) {
+    //     // the username, password combination is successful
+
+    //     console.log(`succesful login`)
+
+    //     return res.redirect(`/`)
+    // }
+
+    // res.send('Invalid Username/Password');
 })
 
 // starting the server
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`server started at port ${process.env.PORT}`)
 })
