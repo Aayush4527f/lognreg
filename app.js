0e5603d0c2f33ff1130d55790726c1c82ea04066
Aayush <aayushchhajed@gmail.com>
2022-03-06 20:40:22 +0530

    static files, view engine, parsers

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ const mongoose = require('mongoose')
 const path = require('path')
 const User = require('./model/user')
 const PORT = 80
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.set('view engine', 'ejs')
